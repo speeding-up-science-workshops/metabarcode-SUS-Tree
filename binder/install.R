2550880bf40177eb8dd4e40dd7b59cd285723006
NielInfante <aminfante@hsc.wvu.edu>
2019-10-23 17:16:42 -0400

    Add files via upload

---
+++ binder/install.R
@@ -0,0 +1,5 @@
+pkgs = c("reshape2", "tidyverse", "plotly","upsetr","ggpubr","knitr", "CaTools", "bitops", "rprojroot", "rmarkdown","scales","gplots")
+ncores = parallel::detectCores()
+install.packages(pkgs, Ncpus = ncores)
+source('http://bioconductor.org/biocLite.R')
+biocLite('phyloseq')
