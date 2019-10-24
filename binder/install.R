819c217540d8ff27fff21cb5c870f89ca52538e7
NielInfante <aminfante@hsc.wvu.edu>
2019-10-24 13:40:10 -0400

    Change to BiocManager

--- binder/install.R
+++ binder/install.R
@@ -1,5 +1,6 @@
-pkgs = c("reshape2", "tidyverse", "plotly","upsetr","ggpubr","knitr", "CaTools", "bitops", "rprojroot", "rmarkdown","scales","gplots")
+pkgs = c("BiocManager", "tidyverse", "plotly","knitr", "rprojroot", "rmarkdown","scales","taxize", "data.table")
 ncores = parallel::detectCores()
 install.packages(pkgs, Ncpus = ncores)
-source('http://bioconductor.org/biocLite.R')
-biocLite('phyloseq')
+
+BiocManager::install("phyloseq")
+
