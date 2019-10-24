6ad9505e18c6729b4bce1f39024dccc1fcaf7690
Your Name <you@example.com>
2019-10-24 13:55:59 -0400

    Add makeTree.R

--- makeTree.R
+++ makeTree.R
@@ -4,100 +4,66 @@ library(plotly)
 library(data.table)
 
 library(taxize)
+library(ape)
+
+# For the data we picked, no phylogenetic tree was provided, so I constructed one based on the classifications
+# Normally, its better to do this from the sequence itself, but no sequence was provided either.
+# In order ro get branch lengths, we need to query an outside database. I choose NCBI, because they did provide
+# NCBI IDs for most of the taxa. 
+# There are repeats and missing data, so the number of taxa is reduced greatly in this process. Since I was 
+# mostly interested in creating a tree for a test visualization, I wasn't too concerned. If you are interested in 
+# learning Biology from the resulting tree, you'll have to be a lot more careful.
 
 
-library("reshape2")
-library("tidyr")
-library("plyr")
-library("data.table")
-library("zoo")
-library("taxize")
-library("ape")
 
-
-
-#otu <- read_csv('Data_Files/otu.csv')
-
+# Get Taxa
 taxa <- read_csv('Data_Files/taxa.csv')
 
-
+# Get list of NCBI IDs
 ncbi <- read_csv('Data_Files/OM.CompanionTables.csv')
 names(ncbi)[1] <- 'NCBI_ID'
 
-
-
-filter(ncbi,`NCBI taxonomy ID` == 64091)
-
-
+# Merge tables together
 d <- merge(ncbi, taxa)
 d <- as_tibble(d)
 
 d <- arrange(d, NCBI_ID)
 
+# Get rid of duplicates
 ncbi_list <- d[!duplicated(d$NCBI_ID),]
 ncbi_list <- ncbi_list[!duplicated(ncbi_list$X1),]
 
+Sys.setenv(ENTREZ_KEY = "Your NCBI API Key")
+
+# Query NCBI to get needed info
 tax_class <- classification(ncbi_list$NCBI_ID, db = "ncbi")
+
+# Make the tree
 tax_tree <- class2tree(tax_class, check = TRUE)
+
+# Test that that all worked
 plot(tax_tree)
 
 
 
-
-nl <- ncbi_list$NCBI_ID
-
-
-nl1 <- nl[c(1:125,150:236)]
-nl1 <- nl[150:236]
-
-tc1 <- classification(nl1, db='ncbi')
-tax_tree <- class2tree(tc1, check = TRUE)
-
-tcn <- names(tc1)
-
-length(tcn)
+# I just want the tree part
+phylo <- tax_tree$phylo
 
 
-
-taxize::use_entrez()
-
-b77eef804d3cfdf79c86943588dc52449508 
-
-
-Sys.setenv(ENTREZ_KEY = "b77eef804d3cfdf79c86943588dc52449508")
-
-
-
-
-head(ncbi_list)
-
-filter(ncbi_list, Species == "Desulfospira joergensenii DSM 10085")
-filter(ncbi_list, Genus == "Desulfospira") %>% select(Species)
-filter(ncbi_list,X1=='Seq6721')
-
+# Give the tips names that I want. This is a little convoluted, and can probably be cleaned up
+# The tip labels have to be the same as in th taxa table, and the OTU table
 treeNames <- tax_tree$names
 tn <- data.frame(NCBI_ID = treeNames)
 tn$NCBI_ID <- as.character(tn$NCBI_ID)
 tn$NCBI_ID <- as.numeric(tn$NCBI_ID)
 
-head(ncbi_list)
-
 seqList <- data.frame(NCBI_ID = ncbi_list$NCBI_ID, Seq=ncbi_list$X1)
 
-head(seqList)
-
 l2 <- left_join(tn, seqList)
-
-l2
-dim(tn)
-
-tax_tree$names <- l2$Seq
-
-
-phylo <- tax_tree$phylo
 
 phylo$tip.label <- l2$Seq
 
 
-write.tree(phylo, file="Data_Files/tree.tre")
+# Write the tree
+ape::write.tree(phylo, file="Data_Files/tree.tre")
 
